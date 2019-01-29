53b9cb0af57877bc50ed72501b9dc96fabf7b45b
zqzneptune <zqzneptune@hotmail.com>
2019-01-29 17:05:59 -0600

    Adding HG document and rename colum names

--- R/HG.R
+++ R/HG.R
@@ -1,10 +1,23 @@
-#' HG: Calculates HG scoresa given input table of AP-MS runs
+#' HGScore
+#' Scoring algorithm based on a hypergeometric distribution error model (Hart et al.,
+#'  2007) with incorporation of NSAF (Zybailov, Boris, et al., 2006). This algorithm was first introduced to
+#'  predict the protein complex network of Drosophila melanogaster
+#'  (Guruharsha, K. G., et al., 2011).
+#'
+#' @title HGScore
+#' @param datInput A dataframe with column names: idRun, idPrey, countPrey, lenPrey.
+#' Each row represent one unique protein captured in one pull-down experiment.
 #'
 #'
-#' @title HG
-#' @param datInput A table of Run_id, Prey, Spectral counts, Protein Length.
-#' @return A data frame containing the following columns:
-#'         InteractorA, InteractorB, HG scores
+#' @return A dataframe consists of pairwise combindation of preys identified in the input with HG scores
+#' indicating interacting probabilities computed from negative log transformed
+#' Hypergeometric test P-values.
+#'
+#' @author Qingzhou Zhang, \email{zqzneptune@hotmail.com}
+#' @references Guruharsha, K. G., et al. "A protein complex network of Drosophila melanogaster." Cell 147.3 (2011): 690-703.\url{https://doi.org/10.1016/j.cell.2011.08.047}
+#' @references Hart, G. Traver, Insuk Lee, and Edward M. Marcotte. "A high-accuracy consensus map of yeast protein complexes reveals modular nature of gene essentiality." BMC bioinformatics 8.1 (2007): 236.\url{https://doi.org/10.1186/1471-2105-8-236}
+#' @references Zybailov, Boris, et al. "Statistical analysis of membrane proteome expression changes in Saccharomyces c erevisiae." Journal of proteome research 5.9 (2006): 2339-2347.\url{https://doi.org/10.1021/pr060161n}
+
 #' @importFrom dplyr group_by
 #' @importFrom dplyr summarise
 #' @importFrom dplyr mutate
@@ -22,13 +35,13 @@
 #' @useDynLib ppiAPMS
 #' @exportPattern "^[[:alpha:]]+"
 #' @export
-#' @author Qingzhou Zhang
+
 
 HG <- function(datInput){
   . <- NULL
-  Run_id <- NULL
-  Peptide_cnt <- NULL
-  Length <- NULL
+  idRun <- NULL
+  countPrey <- NULL
+  lenPrey <- NULL
   NormalSpec <- NULL
   SumNS <- NULL
   NSAF <- NULL
@@ -41,22 +54,22 @@ HG <- function(datInput){
   HG <- NULL
   datCnt <-
     datInput %>%
-      mutate(`NormalSpec` = `Peptide_cnt`/`Length`) %>%
-      group_by(`Run_id`) %>%
+      mutate(`NormalSpec` = `countPrey`/`lenPrey`) %>%
+      group_by(`idRun`) %>%
       mutate(`SumNS` = sum(`NormalSpec`)) %>%
       mutate(`NSAF` = `NormalSpec`/`SumNS`) %>%
-      group_by(`Run_id`) %>%
+      group_by(`idRun`) %>%
       mutate(`NormalNSAF` = `NSAF`/min(`NSAF`)) %>%
       mutate(`Tn` = as.integer(sqrt(`NormalNSAF`)))
   d <-
-    spread(datCnt[, c("Run_id", "Prey", "Tn")], `Run_id`, `Tn`)
+    spread(datCnt[, c("idRun", "idPrey", "Tn")], `idRun`, `Tn`)
   g <-
     as.matrix(d[, -1])
   g[is.na(g)] <- 0
   rownames(g) <-
-    d$Prey
+    d$idPrey
   pps <-
-    combn(d$Prey, 2)
+    combn(d$idPrey, 2)
   PPN <-
     GetPPN(t(g))
   CppPPN <-
@@ -95,6 +108,6 @@ HG <- function(datInput){
       left_join(., sumMinTnInteractorB, by = "InteractorB") %>%
       mutate(`NMinTn` = sum(tnProtein$minTn)/2) %>%
       mutate(`HG` = -phyper(`ppiTN`, `tnA`, `NMinTn`-`tnB`,`tnB`,
-                            lower.tail = FALSE, log.p = TRUE))
-  return(scorePPI)
+                            lower.tail = FALSE, log.p = TRUE)) # Natural Logrithm
+  return(scorePPI[, c("InteractorA", "InteractorB", "HG")])
 }
